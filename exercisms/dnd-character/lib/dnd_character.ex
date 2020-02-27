444a1800896f620eb4ebcec324308962fea776fa
kipernicus <kip.armstrong@gmail.com>
2020-02-27 10:58:28 -0700

    Solution

--- exercisms/dnd-character/lib/dnd_character.ex
+++ exercisms/dnd-character/lib/dnd_character.ex
@@ -13,13 +13,33 @@ defmodule DndCharacter do
 
   @spec modifier(pos_integer()) :: integer()
   def modifier(score) do
+    trunc(Float.floor((score-10)/2))
+  end
+
+  @spec roleD6 :: pos_integer()
+  def roleD6 do
+    Enum.random([1, 2, 3, 4, 5, 6])
   end
 
   @spec ability :: pos_integer()
   def ability do
+    rolls = Enum.sort([roleD6(), roleD6(), roleD6(), roleD6()])
+    highest = List.delete_at(rolls, 0)
+    Enum.reduce(highest, 0, fn element, accumulator -> element + accumulator end)
   end
 
   @spec character :: t()
   def character do
+    constitution = ability()
+    hitpoints = 10+modifier(constitution)
+    %DndCharacter{
+      strength: ability(),
+      dexterity: ability(),
+      constitution: constitution,
+      intelligence: ability(),
+      wisdom: ability(),
+      charisma: ability(),
+      hitpoints: hitpoints
+    }
   end
 end
